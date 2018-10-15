c91bee08d9b2156cc14e109f9a92eb980ababe88
wang <664322480@qq.com>
2018-10-15 18:13:08 +0800

    webpack-dev-middleware ；接下来是一个 webpack-dev-middleware 配合 express server 的示例。

--- webpack.config.js
+++ webpack.config.js
@@ -8,9 +8,9 @@ module.exports = {
     print:'./src/print.js'
   },
   devtool: 'inline-source-map',
-  devServer:{
-    contentBase: './dist'
-  },
+  // devServer:{
+  //   contentBase: './dist'
+  // },
   plugins:[
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
@@ -20,40 +20,41 @@ module.exports = {
   output:{
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
-  module:{
-    rules:[
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test:/\.(png|svg|jpg|gif)$/,
-        use:[
-          'file-loader'
-        ]
-      },
-      {
-        test:/\.(woff|woff2|eot|ttf|otf)$/,
-        use:[
-          'file-loader'
-        ]
-      },
-      {
-        test:/\.(csv|tsv)$/,
-        use:[
-          'csv-loader'
-        ]
-      },
-      {
-        test:/\.xml$/,
-        use:[
-          'xml-loader'
-        ]
-      }
-    ]
-  }
+  // module:{
+  //   rules:[
+  //     {
+  //       test: /\.css$/,
+  //       use: [
+  //         'style-loader',
+  //         'css-loader'
+  //       ]
+  //     },
+  //     {
+  //       test:/\.(png|svg|jpg|gif)$/,
+  //       use:[
+  //         'file-loader'
+  //       ]
+  //     },
+  //     {
+  //       test:/\.(woff|woff2|eot|ttf|otf)$/,
+  //       use:[
+  //         'file-loader'
+  //       ]
+  //     },
+  //     {
+  //       test:/\.(csv|tsv)$/,
+  //       use:[
+  //         'csv-loader'
+  //       ]
+  //     },
+  //     {
+  //       test:/\.xml$/,
+  //       use:[
+  //         'xml-loader'
+  //       ]
+  //     }
+  //   ]
+  // }
 };
