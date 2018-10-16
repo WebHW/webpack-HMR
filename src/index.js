e59388424f8021c0e1e9cdbc74c7dafb94968adb
wang <664322480@qq.com>
2018-10-16 11:24:25 +0800

    由于 import() 会返回一个 promise，因此它可以和 async 函数一起使用。但是，需要使用像 Babel 这样的预处理器和Syntax Dynamic Import Babel Plugin。下面是如何通过 async 函数简化代码：

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import { cube } from './another-module';
 
-function getComponent() {
-
-  return import(/* webpackChunkName: "lodash" */ 'lodash').then(
-    _ => {
-      var element = document.createElement('div');
-      element.innerHTML = _.join(['Hello', 'webpack'], '');
-      return element;
-    }
-  ).catch(error => 'An error occurred while loading the component');
+async function getComponent(){
+  var element = document.createElement('div');
+  const _ = await import(/* webpackChunkName: "lodash" */ 'lodash');
+  element.innerHTML = _.join(['Hello', 'webpack'], '');
+  return element;
 }
 
 getComponent().then(component => {
