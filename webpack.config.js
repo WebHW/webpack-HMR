46c0a094ba7bd45d5918a10bdcd77f566d1caab8
wang <664322480@qq.com>
2018-10-16 09:59:53 +0800

    HMR 修改样式表

--- webpack.config.js
+++ webpack.config.js
@@ -12,52 +12,26 @@ module.exports = {
     contentBase: './dist',
     hot: true
   },
+  module:{
+    rules:[
+      {
+        test:/\.css$/,
+        use:[
+          'style-loader',
+           'css-loader'
+          ]
+      }
+    ]
+  },
   plugins:[
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title:' Output Management'
+      title:'Hot Module Management'
     }),
-    new webpack.NamedModulesPlugin(),
     new webpack.HotModuleReplacementPlugin()
   ],
   output:{
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
-  },
-  // module:{
-  //   rules:[
-  //     {
-  //       test: /\.css$/,
-  //       use: [
-  //         'style-loader',
-  //         'css-loader'
-  //       ]
-  //     },
-  //     {
-  //       test:/\.(png|svg|jpg|gif)$/,
-  //       use:[
-  //         'file-loader'
-  //       ]
-  //     },
-  //     {
-  //       test:/\.(woff|woff2|eot|ttf|otf)$/,
-  //       use:[
-  //         'file-loader'
-  //       ]
-  //     },
-  //     {
-  //       test:/\.(csv|tsv)$/,
-  //       use:[
-  //         'csv-loader'
-  //       ]
-  //     },
-  //     {
-  //       test:/\.xml$/,
-  //       use:[
-  //         'xml-loader'
-  //       ]
-  //     }
-  //   ]
-  // }
+    path: path.resolve(__dirname, 'dist')
+  }
 };
