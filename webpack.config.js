f1a60efeb8957f947e63c47d4931717892efe51d
wang <664322480@qq.com>
2018-10-31 16:13:56 +0800

    设定 HtmlWebpackPlugin，

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     app:'./src/index.js',
     print:'./src/print.js'
   },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title:'Output Mangement'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
   }
 };
