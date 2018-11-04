ce86a7bd23a2ffb41add54fff4970590bee7fac2
WebHW <664322480@qq.com>
2018-11-04 21:06:28 +0800

    预先准备

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
+
 function component(){
   var element = document.createElement('div');
-  
+  var btn = document.createElement('button');
   // Lodash, now imported by this script
   element.innerHTML = _.join(['Hello','webpack'], ' ');
+  
+  btn.innerHTML ='click me and check the conole!';
+
+  btn.onclick= printMe;
+  element.appendChild(btn);
   return element;
 }
 
