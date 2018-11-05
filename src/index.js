04310d351d1db2da57b53d8fd25c0ad7bc566391
WebHW <664322480@qq.com>
2018-11-05 15:49:15 +0800

    还要注意，任何位于 /src 的本地代码都可以关联到 process.env.NODE_ENV 环境变量，所以以下检查也是有效的：

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
+// 还要注意， 任何位于 / src 的本地代码都可以关联到 process.env.NODE_ENV 环境变量， 所以以下检查也是有效的：
 import {
   cube
 } from './math.js';
 
+if(process.env.NODE_ENV !== 'production'){
+  console.log('Looks like we are in development mode !');
+}
 function component() {
   var element = document.createElement('pre');
   element.innerHTML = [
