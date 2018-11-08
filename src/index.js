e41103455fa238cc5e76d808221d6d71d4db3ef8
wang <664322480@qq.com>
2018-11-08 17:36:41 +0800

    模块热替换，启用 HMR

--- src/index.js
+++ src/index.js
@@ -5,14 +5,20 @@ function component() {
   var element = document.createElement("div");
   var btn = document.createElement("button");
 
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["hello", "webpack"], "");
 
-  btn.innerHTML = "Click me and check the console!";
+  btn.innerHTML = "click me and check the console!";
   btn.onclick = printMe;
 
   element.appendChild(btn);
-
   return element;
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept("./print.js", function() {
+    console.log("Accepting this updated printMe module!");
+    printMe();
+  });
+}
